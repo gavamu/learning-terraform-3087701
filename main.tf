a3132a5df26cca8ffcdfe8b2ba131f505aee6812
jmuela1015 <jeremiah.muela@gmail.com>
2026-03-17 22:32:05 -0700

    3_7 use module for security group - clean up

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
-  name    = "blog_new"
+  name    = "blog"
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -41,38 +41,4 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
   vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  to_port           = 443
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  from_port         = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  to_port           = 80
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.blog.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
 }
